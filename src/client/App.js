59dacc81af191daf2ebe367240d197349a60c0b6
Adrian Christensen <adri0477@gmail.com>
2020-01-29 18:53:03 +0100

    .

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './app.css';
 
-import { AppBar, Toolbar, Typography, Tabs, Tab } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Tabs, Tab, Box } from '@material-ui/core';
 
 export default class App extends Component {
 	componentDidMount() {}
@@ -14,13 +13,6 @@ export default class App extends Component {
 						<Typography variant="h5" color="inherit">
 							Adrinet
 						</Typography>
-						<Tabs
-							indicatorColor="primary"
-							textColor="primary"
-							centered
-						>
-							<Tab label="Home" />
-						</Tabs>
 					</Toolbar>
 				</AppBar>
 			</div>
