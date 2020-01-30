c78dfa72802dd8de50ea30efdfef76ac1d2133be
adri0477 <adri0477@gmail.com>
2020-01-30 09:12:57 +0100

    ...

--- src/client/Components/Layouts/Header.js
+++ src/client/Components/Layouts/Header.js
@@ -1,18 +1,30 @@
+/* eslint-disable arrow-parens */
 import React, { Component } from 'react';
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import TypoGraphy from '@material-ui/core/Typography';
+import { AppBar, Toolbar, Typography, Tab, Tabs } from '@material-ui/core';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { selectedTab: '0' };
+	}
+
+	handleTabChange = (object, index) => this.setState({ selectedTab: index });
+
 	render() {
+		const { selectedTab } = this.state;
+
 		return (
 			<AppBar color="primary" position="static">
 				<Toolbar>
-					<TypoGraphy variant="h5" color="inherit">
+					<Typography variant="h6" color="inherit">
 						Adrinet
-					</TypoGraphy>
+					</Typography>
+					<Tabs value={selectedTab} onChange={this.handleTabChange}>
+						<Tab label="HOME" value="0" />
+						<Tab label="ABOUT" value="1" />
+					</Tabs>
 				</Toolbar>
 			</AppBar>
 		);
